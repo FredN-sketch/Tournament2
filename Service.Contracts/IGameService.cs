d1a127de54fda429c0343c5758ed974dea9b2612
Fredrik Norgren <fredrik.norgren@gmail.com>
2025-07-01 15:47:11 +0200

    Added Service.Contracts (with interfaces) and Tournament.Services

---
+++ Service.Contracts/IGameService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tournament.Core.Entities;
+
+namespace Service.Contracts
+{
+    public interface IGameService
+    {
+        Task<IEnumerable<Game>> GetAllAsync(bool sortByTitle = false);
+        Task<Game?> GetAsync(int id);
+        Task<Game?> GetAsync(string title);
+        Task<bool> AnyAsync(int id);
+    }
+}
---
+++ Service.Contracts/ITournamentService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tournament.Core.Entities;
+
+namespace Service.Contracts
+{
+    public interface ITournamentService
+    {
+        Task<IEnumerable<TournamentDetails>> GetAllAsync(bool includeGames = false, bool sortByTitle = false);
+        Task<TournamentDetails?> GetAsync(int id, bool includeGames = false);
+        Task<bool> AnyAsync(int id);
+    }
+}
